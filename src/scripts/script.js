b46a0a3a0bbf0f5560582a436011cad81d2da205
Sonia Fabre <sonia.fabre@gmail.com>
2015-07-30 12:20:42 +0200

    First draft

---
+++ src/scripts/script.js
@@ -0,0 +1,16 @@
+var jQuery = require('jquery');
+
+(function($) {
+  $(function() {
+    var $toggleText = $('#menu-toggle').text();
+    $('#menu-toggle').click(function() {
+      $('#main-menu').slideToggle();
+      $(this).toggleClass('active');
+      if ($(this).text() === $toggleText) {
+        $(this).text('Close');
+      } else {
+        $(this).text($toggleText);
+      }
+    });
+  });
+})(jQuery);
